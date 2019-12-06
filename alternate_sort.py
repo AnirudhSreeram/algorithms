10c312b06c6b028c2c854abb2896ec0d3026b315
Anirudh Sreeram <47698070+AnirudhSreeram@users.noreply.github.com>
2019-12-06 11:11:49 +0530

    Add files via upload
    Alternating sorting algorithm for arrays with complexity of O(n Log n)

--- alternate_sort.py
+++ alternate_sort.py
@@ -7,19 +7,26 @@ Created on Thu Dec 6 2019
 """
 
 def alternateSort (arr,n):
-    arr.sort()
+    arr.sort()   # Sort the array in ascending order. 
     print(arr)
+    
+    # Create pointers for the beginning and the end of the array.
     hi = n-1
     low = 0
     
-    while(hi > low):
-        print (str(arr[hi]) + " ")
-        hi -=1
-        print (str(arr[low]) + " ")
-        low += 1
-    if (n % 2 != 0):
-        print(arr[low])
     
+    while(hi > low):
+        print (str(arr[hi]), end =" "),
+        print (str(arr[low]), end =" ")
+        hi -=1  # Decrement the end pointer by one.
+        low += 1  # Increment the pointer in the beginning by one.
+        
+    # Odd number of elements are present in the array.    
+    if (n % 2 != 0):
+        print(arr[low], end =" ") # type the middle most number in the end.
+
+
+# Initial inputs.    
 arr = [1, 12, 4, 6, 7, 10]  
 n = len(arr) 
 print(arr)  
